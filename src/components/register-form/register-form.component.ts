2b66adfc98a7a44995732c0940128c18bfbeb0a7
Jack N <jcneligan@gmail.com>
2019-06-13 21:20:15 -0400

    Add firebase module, basic firebase authentication

--- src/components/register-form/register-form.component.ts
+++ src/components/register-form/register-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { ToastController} from 'ionic-angular';
+
+import { Account } from '../../models/account/account.interface';
 
 @Component({
   selector: 'app-register-form',
@@ -6,10 +10,26 @@ import { Component } from '@angular/core';
 })
 export class RegisterFormComponent {
 
-  constructor() {}
+  account = {} as Account;
 
-  register() {
-    
+  constructor(private afAuth: AngularFireAuth, private toast: ToastController) {}
+
+  async register() {
+    try {
+    const result = await this.afAuth.auth.createUserWithEmailAndPassword(this.account.email, this.account.password)
+    this.toast.create({
+      message: 'Account successfully created',
+      duration: 3000
+    }).present();
+    console.log(result);
+    }
+    catch(e) {
+      console.error(e);
+      this.toast.create({
+        message: e.message,
+        duration: 3000
+      }).present();
+    }
   };
 
 }
--- src/pages/register/register.module.ts
+++ src/pages/register/register.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { RegisterPage } from './register';
 import { ComponentsModule } from '../../components/components.module';
 
@@ -9,7 +11,8 @@ import { ComponentsModule } from '../../components/components.module';
   ],
   imports: [
     IonicPageModule.forChild(RegisterPage),
-    ComponentsModule
+    ComponentsModule,
+    AngularFireAuthModule
   ],
 })
 export class RegisterPageModule {}
